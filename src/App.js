d95b0cf78493154e949069433c3d70e264912fbc
Manjunath <122592837+manjunath70360@users.noreply.github.com>
2024-07-16 10:39:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {Route, Switch, BrowserRouter} from 'react-router-dom'
+
+import SignUp from "./components/signup"
+import SignIn from './components/signin'
+import Dashboard from"./components/dashboard"
+import "./App.css"
+
+const App = ()=> {
+return(
+      <BrowserRouter>
+      <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+      </BrowserRouter>
+)
+}
+
+export default App;
